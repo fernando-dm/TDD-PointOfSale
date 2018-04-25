cc07d2a8f2459361f57a0212d1de0ba4d92fd154
Fernando Garcia <fernando.garcia@etermax.com>
2018-04-25 16:12:29 -0300

    recibo un entero y devuelvo un formato string con la moneda y respetando decimales

--- PointOfSale/src/test/java/ca/jbrains/pos/test/FormatPriceTest.java
+++ PointOfSale/src/test/java/ca/jbrains/pos/test/FormatPriceTest.java
@@ -16,8 +16,13 @@ public class FormatPriceTest {
     public static Collection<Object[]>data(){
 
         return Arrays.asList(new Object[][]{
-//                {789, "$7.89"},
-                {0, "$0"}
+                {789, "$7.89"},
+                {0, "$0.00"},
+                {2, "$0.02"},
+                {10, "$0.10"},
+                {100, "$1.00"},
+                {1000, "$10.00"},
+                {1215646845, "$12,156,468.45"}
         });
     }
 
@@ -34,9 +39,10 @@ public class FormatPriceTest {
 
         Assert.assertEquals(formatExpected, formatPrice(formatInCents));
     }
-
-    private String formatPrice(int formatInCents) {
-        return "$0";
+    // debo agregarle al entero formato de divisa 789 ==> 7.89, agrego separador de miles y de cents y luego le concateno $
+    // Esta forma se saca googleando! "$%,.2f"
+    private String formatPrice(int priceInCents) {
+        return String.format("$%,.2f",priceInCents/100.0d);
     }
 
 
